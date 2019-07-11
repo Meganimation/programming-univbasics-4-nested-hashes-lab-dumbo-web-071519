5c28f27a48fc848b0eb5d894c3c84e45fae7f395
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-11 17:37:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,9 +71,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+        :grace_hopper => {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
